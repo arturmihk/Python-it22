2fa6145f9c5c42542702a81faed9ec92cdd4d68d
arturmihk <115464760+arturmihk@users.noreply.github.com>
2022-10-11 11:39:56 +0300

    Add files via upload

---
+++ h03.py
@@ -0,0 +1,18 @@
+#Artur-Mihk Peterson
+#11.10.22
+#harjutus03
+
+#Korralik nimi
+nimi = input("Sisesta nimi: ")
+puh_nimi = nimi.strip().capitalize()
+
+print("Tere,", puh_nimi+"!")
+
+
+
+
+
+
+
+
+
