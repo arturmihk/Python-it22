b8f48a311bd2bd697649e596817de570abeb542c
arturmihk <115464760+arturmihk@users.noreply.github.com>
2022-10-17 14:02:23 +0300

    Add files via upload

---
+++ h04.py
@@ -0,0 +1,73 @@
+#Artur-Mihk Peterson
+#17.10.22
+#harjutus04
+
+#Jalgpalli meeskond
+sugu = input("Sisesta sugu m/n: ")
+if sugu==:
+    vanus = input("Sisesta siia oma vanus: ")
+else:
+    print("Sa ei saa jalgpalli tiimi sa oled naine")
+
+
+
+"""
+#müük
+toode = int(input("Sisesta toote hind :"))
+if toode<=10:
+    print("Saad 10% allahindlust")
+else:
+    print("Saad 20% allahindlust")
+
+
+#Juubel
+sunnip = (input("Sisesta siia oma sünnipäev: "))
+dd,mm,yy= sunnip.split(".")
+print(yy)
+aasta = 2022-int(yy)
+jaak = aasta%5
+if jaak==0:
+    print("Sul on juubel")
+else:
+    print("Sul ei ole juubel")
+"""
+
+
+
+'''
+#matem
+arv1 = int(input("Sisesta arv1: "))
+arv2 = int(input("Sisesta arv2: "))
+tehe = input("Vali tehe (* / + -): ")
+if tehe=="*":
+    vastus = arv1*arv2
+elif tehe=="/":
+    vastus = arv1/arv2
+elif tehe=="+":
+    vastus = arv1+arv2
+elif tehe=="-":
+    vastus = arv1-arv2
+
+else:
+    vastus = n/a
+"""
+Mariole meeldib trollida vanaisaga ja vanaemaga
+"""
+print(f"{arv1}{tehe}{arv2}={vastus}")
+print("--------------")
+
+
+
+
+
+#kas on ruut
+a = int(input("Sisesta külg 1: "))
+b = int(input("Sisesta külg 2: "))
+if a==b:
+    print(f"{a} ja {b} moodustavad ruudu")
+else:
+    print(f"{a} ja {b} moodustacad ristküliku")
+'''
+
+
+
