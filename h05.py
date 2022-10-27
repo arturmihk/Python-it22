4eb1664f04077d8f2c755cabaa2c8a10347f5f3f
arturmihk <115464760+arturmihk@users.noreply.github.com>
2022-10-27 09:18:17 +0300

    Add files via upload

--- h05.py
+++ h05.py
@@ -2,9 +2,29 @@
 #25.10.22
 #harjutus05
 
-#Korduste eemaldamine
-opilased = ["Juhan","Kati","Mario","Mario","Mati","Mati"]
+#Tärnid
+numbrid = [12,43,53,11,52,23,69,72,13,32,35]
+for i in numbrid:
+    print(i*"*")
 
+#Vanused
+vanused = [12,43,53,11,52,23,69,72,13,32,35]
+print("Vanim",max(vanused))
+print("Noorim",min(vanused))
+print("Summa",sum(vanused))
+print("Keskmine",sum(vanused)/len(vanused))
+#Dublikaat
+
+opilased = ["Juhan","Kati","Mario","Mario","Mati","Mati"]
+puh_opilased = []
+for i in opilased:
+    if i in opilased:
+        if opilased.count(i) >= 2:
+            opilased.remove(i)
+    #if i not in puh_opilased:
+     #   puh_opilased.append(i)
+        
+print(opilased)
 
 
 #Õpilased
@@ -14,11 +34,11 @@ for i in(opilased):
     print(f"{jrk}. {i}")
     jrk+=1
 #küsi millist tahab muuta
-muut = int(input("Mitmendat nime sa muuta tahad?"))
+muut = int(input("Millist nime sa muuta tahad?"))
 uusn = input("Milliseks soovite nime muuta?")
 opilased.remove(opilased[muut])
 opilased.insert(muut, uusn)
-
+#tee muudatus
 
 print(opilased)
     
@@ -29,6 +49,5 @@ for i in range(5):
     nimi = input("Lisa nimi loendisse: ")
     nimed.append(nimi)
 nimed.sort()
-
 print(nimed)
 print(f"viimati lisatud nimi: {nimed[-1]}")
