600f3bcb6809824e9ecb2a429773ebc1e748f640
arturmihk <115464760+arturmihk@users.noreply.github.com>
2022-11-21 14:02:21 +0200

    Add files via upload

---
+++ töö.py
@@ -0,0 +1,23 @@
+#Artur-Mihk Peterson
+#21.11.22
+#töö
+#5. Kaugushüpe
+#kasutaja sisestab 3 kaugushüppe tulemust
+#sinu programm leiab ning väljastab parima ja keskmise tulemuse
+#programmi dialoog kasutajaga on arusaadav ja üheselt mõistetav
+#kogu looming salvestatakse tekstifaili
+#kood kommenteeritud
+
+def kaugush(esi, teine, kolmas):
+    with open("har5.txt", "w") as f:
+        suurim = max(esi,teine,kolmas)
+        avg = round((esi+teine+kolmas)/3)
+        print(f"Sinu parim hüpe oli {suurim}")
+        print(f"Sinu keskmine tulemus oli {avg}")
+        return suurim
+        return avg
+
+esih = int(input("Sisesta siia oma esimene hüpe: "))
+teineh = int(input("Sisesta siia oma teine hüpe: "))
+kolmash = int(input("Sisesta siia oma kolmas hüpe: "))
+kaugush(esih, teineh, kolmash)
